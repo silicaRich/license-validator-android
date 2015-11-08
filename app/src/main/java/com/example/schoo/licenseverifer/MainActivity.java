27869e321329ecab8b0d45e0e91da488287cea2f
Nicole Flokos <nflokos@cogstate.com>
2015-11-08 02:21:21 -0500

    pushing what I have for now,
    :dancers:

--- app/src/main/java/com/example/schoo/licenseverifer/MainActivity.java
+++ app/src/main/java/com/example/schoo/licenseverifer/MainActivity.java
@@ -14,30 +14,36 @@ import android.app.Activity;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "NfcDemo";
 
-    private TextView mTextView;
+    //    private TextView mTextView;
+    private Button signupButton;
     private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //etSupportActionBar(toolbar);
 
-        mTextView = (TextView) findViewById(R.id.first_name_label);
+
+
+        //mTextView = (TextView) findViewById(R.id.first_name_label);
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-           //   mTextView.setText("NFC is disabled.");
+        //   mTextView.setText("NFC is disabled.");
 
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -48,23 +54,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (!mNfcAdapter.isEnabled()) {
-      //      mTextView.setText("NFC is disabled.");
+            //      mTextView.setText("NFC is disabled.");
         } else {
-        //    mTextView.setText(R.string.explanation);
+            //    mTextView.setText(R.string.explanation);
             //mTextView.setText("NFC is enabled");
         }
 
         handleIntent(getIntent());
         initFields();
 
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        signupButton = (Button) findViewById(R.id.signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                initFields();
             }
-        });*/
+        });
     }
 
     private void handleIntent(Intent intent) {
@@ -72,20 +76,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Initialize fields for License
-    private void initFields(){
-        final String licenseNumber = "";
-        TextView firstName = (TextView) findViewById(R.id.first_name_label);
-        firstName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                boolean handled = false;
-                if (actionId == EditorInfo.IME_NULL) {
-                   // licenseNumber = v.getText();
-                    handled = true;
-                }
-                return handled;
-            }
-        });
+    private void initFields() {
+
+        EditText firstName = (EditText) findViewById(R.id.first_name_text);
+        EditText lastName = (EditText) findViewById(R.id.first_name_text);
+        EditText dob = (EditText) findViewById(R.id.date_of_birth_text);
+        EditText address = (EditText) findViewById(R.id.first_name_text);
+    //    EditText city = (EditText) findViewById(R.id.city_text);
+        EditText state = (EditText) findViewById(R.id.state_text);
+        EditText zipcode = (EditText) findViewById(R.id.zip_text);
+        EditText licenseNumber = (EditText) findViewById(R.id.license_number_text);
+        String endpoint = "?dl_sn=" + licenseNumber + "&fullname=" + firstName + lastName
+                + "&street1=" + "&zip=" + zipcode + "&dob=" + dob;
+        enrollment(endpoint);
+    }
+
+    // Api Calls
+    private void enrollment(String endpoint){
+
+        String request = "http://54.174.128.27:8123" + endpoint;
+
+        StringRequest myReq = new StringRequest(Method.POST,
+                "http://ave.bolyartech.com/params.php",
+                createMyReqSuccessListener(),
+                createMyReqErrorListener()) {
+
+            protected Map<string, string=""> getParams() throws com.android.volley.AuthFailureError {
+                Map<string, string=""> params = new HashMap<string, string="">();
+                params.put("param1", num1);
+                params.put("param2", num2);
+                return params;
+            };
+        };
+        queue.add(myReq);
+
+
+
+
     }
 /*
     @Override
