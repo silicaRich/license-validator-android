d233ee50ba1687fd7da5ba636bd69238cba74a81
Nicole Flokos <nflokos@cogstate.com>
2015-11-07 20:34:40 -0500

    sihtty code that is all commented out
    and has only one textfield

--- app/src/main/java/com/example/schoo/licenseverifer/MainActivity.java
+++ app/src/main/java/com/example/schoo/licenseverifer/MainActivity.java
@@ -6,14 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,12 +31,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //etSupportActionBar(toolbar);
 
-        mTextView = (TextView) findViewById(R.id.textView_explanation);
+        mTextView = (TextView) findViewById(R.id.first_name_label);
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
+           //   mTextView.setText("NFC is disabled.");
 
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -42,28 +48,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (!mNfcAdapter.isEnabled()) {
-            mTextView.setText("NFC is disabled.");
+      //      mTextView.setText("NFC is disabled.");
         } else {
-            mTextView.setText(R.string.explanation);
+        //    mTextView.setText(R.string.explanation);
             //mTextView.setText("NFC is enabled");
         }
 
         handleIntent(getIntent());
+        initFields();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     private void handleIntent(Intent intent) {
         // TODO: handle Intent
     }
 
+    // Initialize fields for License
+    private void initFields(){
+        final String licenseNumber = "";
+        TextView firstName = (TextView) findViewById(R.id.first_name_label);
+        firstName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_NULL) {
+                   // licenseNumber = v.getText();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,5 +108,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
